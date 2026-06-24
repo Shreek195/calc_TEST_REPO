8e12b6eef02eb28af32f6b2ec4d71016f96869d1
Shreek195 <shreekoshti199@gmail.com>
2026-06-24 09:29:34 +0530

    Subtraction added to main.py

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ num1 = int(input("Enter number 1: "))
 num2 = int(input("Enter number 2: "))
 
 print(f"Addition of {num1} and {num2} = {num1 + num2}")
+print(f"Subtraction of {num1} and {num2} = {num1 - num2}")
